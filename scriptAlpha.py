2b302f6c6738ca6577c9cc591b6838a11ab8bff4
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-08-11 20:06:11 +0000

    Configuring mypy and setting up a bad type situation

--- scriptAlpha.py
+++ scriptAlpha.py
@@ -1,3 +1,4 @@
+from typing import List
 
 print("Welcome to scriptAlpha")
 
@@ -9,3 +10,11 @@ for i in range(10):
 print(f"{variableX=}")
 
 
+def addNumbers(xl: List[int]):
+    return(sum(xl))
+
+nums = [1,2,3,4,5]
+lets = ['a', 'b', 'c']
+print(f"Adding some numbers {addNumbers(nums)}")
+
+print(f"Adding some letters {addNumbers(lets)}") ## mypy will catch this
