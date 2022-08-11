ec7503c4ac126fb8a215989d17c0bcf9198dd7c2
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-08-11 18:41:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python and VSCode
+
+See: https://code.visualstudio.com/docs/python/debugging
+
+
+
+# Steps to get started
---
+++ scriptAlpha.py
@@ -0,0 +1,9 @@
+
+print("Welcome to scriptAlpha")
+
+variableX = 10
+
+for i in range(10):
+    varyableX = variableX + i * 10
+
+print(f"{variableX=}")
